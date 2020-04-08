a2bb61a714a4c435fbab2ed6433b71375e35628e
jasonren5 <jasonren72@gmail.com>
2020-04-08 14:25:07 -0400

    added references to firebaseauth and firestore instances in TreeActivity.kt

--- genealogy_app/app/src/main/java/com/example/genealogy_app/Activities/TreeActivity.kt
+++ genealogy_app/app/src/main/java/com/example/genealogy_app/Activities/TreeActivity.kt
@@ -6,10 +6,17 @@ import com.example.genealogy_app.Adapters.TabAdapter
 import com.example.genealogy_app.R
 import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_tree.*
 
 //this is the main activity, has three tabs at the bottom
 class TreeActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +25,12 @@ class TreeActivity : AppCompatActivity() {
         var tabLayoutAdapter = TabAdapter(supportFragmentManager)
         viewPager.adapter = tabLayoutAdapter
 
+
+        auth = FirebaseAuth.getInstance()
+        db = Firebase.firestore
+        
+
+        //db.collection("users") is indexed by email as the key, so you can query the users collection with auth.currentUser!!.email
+        //  to get the current user's document
     }
 }
