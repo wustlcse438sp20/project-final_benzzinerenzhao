fb2271ba894b590b2b7e21297c0170923f0b8479
jasonren5 <jasonren72@gmail.com>
2020-04-08 14:20:34 -0400

    added tablayout with viewpager to tree activity, created three blank fragments

--- genealogy_app/app/src/main/java/com/example/genealogy_app/Activities/TreeActivity.kt
+++ genealogy_app/app/src/main/java/com/example/genealogy_app/Activities/TreeActivity.kt
@@ -2,7 +2,11 @@ package com.example.genealogy_app.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.genealogy_app.Adapters.TabAdapter
 import com.example.genealogy_app.R
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_tree.*
 
 //this is the main activity, has three tabs at the bottom
 class TreeActivity : AppCompatActivity() {
@@ -10,6 +14,9 @@ class TreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tree)
+
+        var tabLayoutAdapter = TabAdapter(supportFragmentManager)
+        viewPager.adapter = tabLayoutAdapter
 
     }
 }
---
+++ genealogy_app/app/src/main/java/com/example/genealogy_app/Adapters/TabAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.genealogy_app.Adapters
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.genealogy_app.Fragments.HomeFragment
+import com.example.genealogy_app.Fragments.ProfileFragment
+import com.example.genealogy_app.Fragments.TreeListFragment
+
+class TabAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getCount(): Int  = 3
+
+    override fun getItem(i: Int): Fragment {
+        var fragment: Fragment
+
+        when(i) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = TreeListFragment()
+            2 -> fragment = ProfileFragment()
+            else -> fragment = HomeFragment()
+        }
+
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        when(position) {
+            0 -> return "Home"
+            1 -> return "Trees"
+            2 -> return "Profile"
+            else -> return "error"
+        }
+    }
+}
