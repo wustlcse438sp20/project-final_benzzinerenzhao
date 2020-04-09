11c5f42767d8b573f99616d1dbdfa8769ee7375b
jasonren5 <jasonren72@gmail.com>
2020-04-09 12:13:35 -0400

    profile fragment now grabs data from firestore and displays it

--- genealogy_app/app/src/main/java/com/example/genealogy_app/Fragments/ProfileFragment.kt
+++ genealogy_app/app/src/main/java/com/example/genealogy_app/Fragments/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.genealogy_app.Fragments
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +11,13 @@ import android.view.ViewGroup
 
 import com.example.genealogy_app.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
+
+    val TAG: String = "ProfileFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +36,40 @@ class ProfileFragment : Fragment() {
         var auth = FirebaseAuth.getInstance()
         var email = auth.currentUser!!.email
 
+        var db = FirebaseFirestore.getInstance()
+        db.collection("users").document(email!!).get()
+            .addOnSuccessListener {document ->
+                if (document != null) {
+                    Log.d(TAG, "Successfully got documentSnapshot")
+                    updateProfileView(document.data!!)
+                }
+                else {
+                    Log.d(TAG, "Successfully queried collection, but document was null")
+                }
 
+            }
+            .addOnFailureListener {
+                Log.d(TAG,"Failed to get documentSnapshot")
+            }
+
+
+    }
+
+    fun updateProfileView(data: Map<String, Any>) {
+        var name = data["firstName"].toString() + " " + data["lastName"].toString()
+        profile_name_text.text = name;
+
+        var dob = data["dob"]
+        if (dob != null && dob.toString() != "")
+            profile_dob.text = "DOB: " + dob.toString()
+
+        var location = data["location"]
+        if (location != null && location.toString() != "")
+            profile_location.text = "Location: " + location.toString()
+
+        var bio = data["bio"]
+        if (bio != null && bio.toString() != "")
+            profile_bio.text = bio.toString()
     }
 
 
